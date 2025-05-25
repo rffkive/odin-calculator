fdf76b07e52d66241cd70f373e50ad8133dbc572
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-05-26 01:59:01 +0800

    rewrite the whole codebase in array to make it work

--- script.js
+++ script.js
@@ -1,80 +1,163 @@
 
 
 function add (a,b) {
-    return c = a +b ;
+    return a +b ;
 }
 
 function product (a,b) {
-    return c = a * b;
+    return a * b;
 }
 
-function substract (a,b) {
-    return c = a - b;
+function subtract (a,b) {
+    return a - b;
 }
 
 function divide (a,b) {
-    return c = a / b;
+    if (b === 0) 
+    { 
+        return "ERROR WAKARIMASEN >.<";
+    }
+    return a / b;
 }
 
-let firstNumber;
-let operator = "";
-let secondNumber;
 
+//function for operation to process
 function operate (operator, a, b) {
     switch (operator) 
     {
         case '+':
           return add(a, b);
         case '-':
-          return substract(a, b);
+          return subtract(a, b);
         case '*':
           return product (a, b);
         case '/':
           return divide(a, b);
-    }
+    };
+    return operations[operator] ? operations[operator](a, b) : b;
 };
 
+// array for calculation logic
+let calculationArray = [];
+let validOperators = ["+","-", "*", "/"];
 
+// calculator state
+let displayValue = "0";
+let AfterEqual = false;
+let AfterOperator = false;
+
+//function to update the display
 const display = document.querySelector("#display");
-const numberBtn = document.querySelectorAll(".number");
 
-let currentDisplay = "";
+function updateDisplay () {
+    display.textContent = displayValue;
+}
+
+//button for number and show in display
+function inputNumber(num) {
+    if (AfterEqual) {
+        calculationArray = [];
+        AfterEqual = false;
+    }  
+    if (AfterOperator) {
+        displayValue = num;
+        AfterOperator = false;
+        console.log("first stop");
+        console.log (displayValue);
+    } 
+    else {
+        displayValue = displayValue === '0' ? num : displayValue + num;
+        console.log("second stop");
+        console.log (displayValue);
+    }
+
+    updateDisplay();
+}
+
+const numberBtn = document.querySelectorAll(".number");
 
 numberBtn.forEach ((button) => {
     button.addEventListener("click", () => {
-        if (operator == "") {
-        currentDisplay += button.textContent; 
-        display.textContent = currentDisplay;
-        firstNumber = parseInt(currentDisplay);
-        } else {
-        currentDisplay += button.textContent; 
-        display.textContent = currentDisplay;
-        secondNumber = parseInt(currentDisplay);
+        inputNumber(button.textContent);
         }
-    });
+    );
 });
+
+//button for operation
+
+function inputOperator (nextOperator) {
+    const inputValue = parseFloat(displayValue);
+
+    if (AfterEqual) {
+        AfterEqual = false;
+    };
+
+    if (calculationArray.length >= 2 && !AfterOperator) {
+        const lastOperator = calculationArray[calculationArray.length - 1];
+        const firstNumber = calculationArray[calculationArray.length - 2];
+        console.log (`${firstNumber} ${lastOperator} ${inputValue}`);
+        const result = operate (lastOperator, firstNumber, inputValue);
+        calculationArray = [result];
+        displayValue = result;
+        console.log(calculationArray);
+        updateDisplay();
+    } else {
+        calculationArray.push(inputValue);
+        console.log(calculationArray);
+    }
+
+    calculationArray.push(nextOperator);
+    console.log(calculationArray);
+    AfterOperator = true;
+}
+
+
 
 const operationBtn = document.querySelectorAll(".operationButton");
 
-
-
 operationBtn.forEach ((button) => {
     button.addEventListener("click", () => {
-        let operatorTemp;
-        operatorTemp += button.textContent;
-        operator = operatorTemp.slice(-1);
-        currentDisplay = "";
+        inputOperator(button.textContent);
     });
 });
 
-const equalBtn = document.querySelector(".equal");
+function expression () {
+        //currentinput;
+        //put currentinput to first value;
+        //operation;
+        //currentiput;
+        //put currentinput to second value;
+        //operate ()
 
-equalBtn.addEventListener("click", () => {
-    operate (operator, firstNumber, secondNumber);
-    display.textContent = c;
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-    currentDisplay = "";
-});
+        //
+};
 
+
+// clear button
+// const clear = document.querySelector(".clear");
+
+// clear.addEventListener ("click", ()=> {
+//     currentInput = '';
+//     expression = [];
+//     updateDisplay('YOUR ANSWER HERE');
+// });
+
+
+//const equalBtn = document.querySelector(".equal");
+
+// equalBtn.addEventListener("click", () => {
+//     console.log(`${firstNumber} ${operator} ${secondNumber}`);
+//     operate (operator, firstNumber, secondNumber);
+//     console.log(`${firstNumber} ${operator} ${secondNumber} = ${total}`);
+    
+//     display.textContent = total;
+//     firstNumber = "";
+//     secondNumber = "";
+//     operator = "";
+//     currentDisplay = "";
+// });
+
+// reduce array
+// first iteration a + b = c
+// second iteration c + b = c
+// third iteration c+ b = c
